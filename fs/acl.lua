7d40f98f805b93c37a39b0bdde5d921dab6b9b52
daiaji <hentaiwushuang@gmail.com>
2025-12-04 14:18:51 +0800

    FixUP

--- fs/acl.lua
+++ fs/acl.lua
@@ -5,20 +5,23 @@ local advapi32 = require 'ffi.req' 'Windows.sdk.advapi32'
 
 local M = {}
 
+-- 暴力重置权限：将 DACL 设为 NULL (Everyone Full Control)
+-- 自动获取所有权和恢复权限
 function M.reset(path)
     token.enable_privilege("SeTakeOwnershipPrivilege")
     token.enable_privilege("SeRestorePrivilege")
     
-    local flags = 5 -- OWNER_SECURITY_INFORMATION | DACL_SECURITY_INFORMATION
+    -- [FIX] Only DACL (4). Do not request OWNER (1) if passing NULL owner.
+    local flags = 4 
     
     local res = advapi32.SetNamedSecurityInfoW(
         util.to_wide(path),
         1, -- SE_FILE_OBJECT
         flags,
-        nil, 
-        nil, 
+        nil, -- Owner (NULL keeps current)
+        nil, -- Group
         nil, -- NULL DACL (Everyone Full Control)
-        nil
+        nil  -- SACL
     )
     
     return res == 0
